d6f8846062f06476193761b4a3e16b2b30b9c4af
hayley <simmons.hayley15@icloud.com>
2022-06-09 17:57:11 -0500

    Added TMDB attribute to footer, set up JS code

--- script.js
+++ script.js
@@ -2,29 +2,25 @@
 // global constants
 
 // API endpoints
-// const q = ""                                                // search phrase/word
-// const limit = 8                                             // number of search result objects to return
-// const offset = 0                                            // starting position of results
-// const rating = "g"                                          // content rating
-// const lang = "en"                                           // default language
-// const api_key = "z9qEJKCAcrrMCA7q77qHJXLDkv111Kra"          // API key
+const api_key = "29ad49e172fb431f874b79cbe0ae49de"        // API key
 // search
 // const apiUrl = `http://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${q}`.toLowerCase()
 
-// form
-const headerDiv_Element = document.getElementById("header-Div")
-const form_Element = document.getElementById("Form")
-const search_Label_Element = document.getElementById("Search-Label")
-const search_Bar_Element = document.getElementById("Search-Bar")
-const search_Button_Element = document.getElementById("Search-Button")
+// page elements
+const headerDiv_Element = document.getElementById("header-Div")              // div labelled header
+const form_H1_Div_Element = document.getElementById("form-H1-div")           // div containing form & 2nd h1 tag
+const form_tag_Element = document.getElementById("Form")                     // form tag
+const search_Label_Element = document.getElementById("Search-Label")         // search label (find all the latest movies...)
+const search_Bar_Element = document.getElementById("Search-Bar")             // search bar
+const search_Button_Element = document.getElementById("Search-Button")       // search button
 
-// results div
+// empty div for results
 const resultsDiv_Element = document.getElementById("results-Div")
 
 // -----------------------------------------
 // results logic
 
-const getResults = form_Element.addEventListener("submit", async (API_results) => {
+const getResults = form_tag_Element.addEventListener("submit", async (API_results) => {
     API_results.preventDefault()
 
     let response = await fetch(apiUrl)
@@ -48,6 +44,6 @@ function displayResults(responseData) {
 // -----------------------------------------
 // search form submit logic
 
-form_Element.addEventListener("submit", (submission) => {
+form_tag_Element.addEventListener("submit", (submission) => {
     
 })
